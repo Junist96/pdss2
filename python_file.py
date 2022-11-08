8ae6ee0b8482f78e38cc7ab447bd8b5c3db7271f
Junist96 <yoonerless@naver.com>
2022-11-08 23:44:41 +0900

    Initial commit

---
+++ python_file.py
@@ -0,0 +1,9 @@
+                                                import numpy as np
+                                                import matplotlib.pyplot as plt
+                                                import pandas as pd
+
+                                                print('안녕 파이썬!!') 
+                                                print("<h1>랜덤하게 점을 찍어봐!</h1>") 
+
+                                                df = pd.read_excel('KRX300.xlsx')
+                                                print(df)
